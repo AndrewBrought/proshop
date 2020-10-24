1299cc2490f0b3b805bf0e788f49d8760339b401
Andrew Brought <andrewbrought@gmail.com>
2020-10-24 13:28:10 -0500

    completed fetching products from db

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -16,16 +18,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-//This gives us a json array with all the products
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
-
-//this gives us a single product by it's id
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000
 
