961bb6ac2233e567f116ae74205cb3751d7da2e5
Andrew Brought <andrewbrought@gmail.com>
2020-10-24 11:15:32 -0500

    prepared sample data

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    // These users have to have only the fields that we setup in our user model or
+    // mongoose wont let us insert into db
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    }
+]
