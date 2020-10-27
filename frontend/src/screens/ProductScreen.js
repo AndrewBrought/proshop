569ad5ee9c3ac69cdfd134b1826255d3b18ae6ce
Andrew Brought <andrewbrought@gmail.com>
2020-10-27 09:12:57 -0500

    added cart landing page and route

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import {listProductDetails} from '../actions/productActions';
 
-const ProductScreen = ({match}) => {
+const ProductScreen = ({ history, match }) => {
     const [qty, setQty] = useState(0);
 
     const dispatch = useDispatch();
@@ -20,98 +20,106 @@ const ProductScreen = ({match}) => {
 
     }, [dispatch, match])
 
+    //Here, we want our click to take us to the cart page with the product id and qty as a query string -
+    // we can use 'history' to push our values over
+    const addToCartHandler = () => {
+        // this should be read as: props.history.push will just redirect with the id and a query string of qty 
+        history.push(`/cart/${match.params.id}?qty=${qty}`);
+    }
+
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
             {loading ?
-                <Loader />
+                <Loader/>
                 : error ?
                     <Message variant='danger'>{error}</Message>
                     : (
-                    <Row>
-                        <Col md={6}>
-                            <Image src={product.image} alt={product.name} fluid/>
-                        </Col>
-                        <Col md={3}>
-                            <ListGroup variant='flush'>
-                                <ListGroup.Item>
-                                    <h3>{product.name}</h3>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Rating
-                                        text={`${product.numReviews} reviews`}
-                                        value={product.rating}
-                                    />
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    Price: ${product.price}
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    Description: {product.description}
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Col>
-                        <Col md={3}>
-                            <Card>
+                        <Row>
+                            <Col md={6}>
+                                <Image src={product.image} alt={product.name} fluid/>
+                            </Col>
+                            <Col md={3}>
                                 <ListGroup variant='flush'>
                                     <ListGroup.Item>
-                                        <Row>
-                                            <Col>
-                                                Price:
-                                            </Col>
-                                            <Col>
-                                                <strong>${product.price}</strong>
-                                            </Col>
-                                        </Row>
+                                        <h3>{product.name}</h3>
                                     </ListGroup.Item>
-
                                     <ListGroup.Item>
-                                        <Row>
-                                            <Col>
-                                                Status:
-                                            </Col>
-                                            <Col>
-                                                {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                                            </Col>
-                                        </Row>
+                                        <Rating
+                                            text={`${product.numReviews} reviews`}
+                                            value={product.rating}
+                                        />
                                     </ListGroup.Item>
-
-                                    {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        Price: ${product.price}
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        Description: {product.description}
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col md={3}>
+                                <Card>
+                                    <ListGroup variant='flush'>
                                         <ListGroup.Item>
                                             <Row>
-                                                <Col>Qty:</Col>
                                                 <Col>
-                                                    <Form.Control as='select' value={qty} onChange={(e) =>
-                                                        setQty(e.target.value)}>
-                                                        {[...Array(product.countInStock).keys()].map(x => (
-                                                            // we increment the key and value by 1 to make sure we're
-                                                            // working with fulfilled value instead of an empty value '0'
-                                                            <option key={x + 1} value={x + 1}>
-                                                                {x + 1}
-                                                            </option>
-                                                        ))}
-                                                    </Form.Control>
+                                                    Price:
+                                                </Col>
+                                                <Col>
+                                                    <strong>${product.price}</strong>
                                                 </Col>
                                             </Row>
                                         </ListGroup.Item>
-                                    )}
 
-                                    <ListGroup.Item>
-                                        <Button
-                                            className='btn-block'
-                                            type='button'
-                                            disabled={product.countInStock === 0}
-                                        >
-                                            Add To Cart
-                                        </Button>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Card>
-                        </Col>
-                    </Row>
-                )}
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>
+                                                    Status:
+                                                </Col>
+                                                <Col>
+                                                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+
+                                        {product.countInStock > 0 && (
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Qty:</Col>
+                                                    <Col>
+                                                        <Form.Control as='select' value={qty} onChange={(e) =>
+                                                            setQty(e.target.value)}>
+                                                            {[...Array(product.countInStock).keys()].map(x => (
+                                                                // we increment the key and value by 1 to make sure we're
+                                                                // working with fulfilled value instead of an empty value '0'
+                                                                <option key={x + 1} value={x + 1}>
+                                                                    {x + 1}
+                                                                </option>
+                                                            ))}
+                                                        </Form.Control>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        )}
+
+                                        <ListGroup.Item>
+                                            <Button
+                                                onClick={addToCartHandler}
+                                                className='btn-block'
+                                                type='button'
+                                                disabled={product.countInStock === 0}
+                                            >
+                                                Add To Cart
+                                            </Button>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Card>
+                            </Col>
+                        </Row>
+                    )}
 
         </>
     )
