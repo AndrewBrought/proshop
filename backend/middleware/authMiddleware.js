fc11b35c50a3b30f798ae7d36ceef7e53e416455
Andrew Brought <andrewbrought@gmail.com>
2020-10-28 00:09:30 -0500

    completed auth middleware setup

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // We split to allow us to break up the return so we have access to the token by itself
+            token = req.headers.authorization.split(' ')[1]
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            // this puts the users data in the req.user minus the password
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized, token failed')
+        }
+    }
+
+    if (!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
+})
+
+export { protect }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-
 const router = express.Router();
 import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile)
+// we implement middleware by putting it as a first argument - so this protect middleware will run when
+// we hit this route
+router.route('/profile').get(protect, getUserProfile)
 
 export default router;
