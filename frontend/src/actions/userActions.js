f5dc1e0575f0b371d81636ad742944c41d232d46
Andrew Brought <andrewbrought@gmail.com>
2020-10-30 23:44:24 -0500

    completed update functionality with dynamic update for name

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,7 +9,7 @@ import {
     USER_LOGOUT,
     USER_REGISTER_FAIL,
     USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS
+    USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS
 } from '../constants/userConstants';
 import {PRODUCT_DETAILS_FAIL} from '../constants/productConstants';
 
@@ -126,6 +126,49 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload:
+            //was not getting my custom error message because I left out 'data' in the condition of my ternary
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
+    }
+}
+
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST,
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+        const {data} = await axios.put(`/api/users/profile`, user, config)
+
+        dispatch({
+            type: USER_UPDATE_PROFILE_SUCCESS,
+            payload: data
+        })
+
+    //    This is how we handle the name in the navbar updating when we update our name
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload:
             //was not getting my custom error message because I left out 'data' in the condition of my ternary
                 error.response && error.response.data.message
