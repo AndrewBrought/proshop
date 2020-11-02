84394ef0d16b3195b9def484b09cca1bd15f4f09
Andrew Brought <andrewbrought@gmail.com>
2020-11-02 10:58:33 -0600

    completed payment screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/cartConstants';
+import {CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD} from '../constants/cartConstants';
 
 // dispatch comes from thunk
 // we're saving our cart to local storage, so we pass getState - which allows us to get our entire state tree
@@ -42,4 +42,13 @@ export const saveShippingAddress = (data) => (dispatch) => {
     })
 
     localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: data
+    })
+
+    localStorage.setItem('paymentMethod', JSON.stringify(data))
 }
---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {Form, Button, Col} from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { savePaymentMethod } from '../actions/cartActions';
+
+const PaymentScreen = ({ history }) => {
+    const cart = useSelector(state => state.cart)
+    const { shippingAddress } = cart
+
+    if(!shippingAddress) {
+        history.push('/shipping')
+    }
+
+    // This is the component level state we're setting for our payment method with default: PayPal
+    const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(savePaymentMethod(paymentMethod))
+        history.push('/placeorder')
+    }
+
+    return <FormContainer>
+        <CheckoutSteps step1 step2 step3 />
+        <h1>Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group>
+                <Form.Label as='legend'>Select Method</Form.Label>
+
+            <Col>
+                {/*PayPal*/}
+                <Form.Check
+                    type='radio'
+                    label='PayPal or Credit Card'
+                    id='PayPal'
+                    name='paymentMethod'
+                    value='PayPal'
+                    checked
+                    onChange={(e) => setPaymentMethod(e.target.value)}
+                >
+
+                </Form.Check>
+
+                 {/* We can add other payments like Stripe*/}
+                {/*<Form.Check*/}
+                {/*    type='radio'*/}
+                {/*    label='Stripe'*/}
+                {/*    id='Stripe'*/}
+                {/*    name='paymentMethod'*/}
+                {/*    value='Stripe'*/}
+                {/*    onChange={(e) => setPaymentMethod(e.target.value)}*/}
+                {/*>*/}
+                {/*</Form.Check>*/}
+
+            </Col>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>
+                Continue
+            </Button>
+        </Form>
+    </FormContainer>
+        ;
+}
+
+export default PaymentScreen;
