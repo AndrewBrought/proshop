4b762f27f598decfa01434295b1535f42c538157
Andrew Brought <andrewbrought@gmail.com>
2020-11-12 11:18:33 -0600

    fixed admin access security and userlist reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,9 +16,10 @@ import {
     USER_DETAILS_RESET,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
-    USER_LIST_FAIL
+    USER_LIST_FAIL,
+    USER_LIST_RESET
 } from '../constants/userConstants';
-import {PRODUCT_DETAILS_FAIL} from '../constants/productConstants';
+
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -63,6 +64,7 @@ export const logout = () => (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
     dispatch({ type: ORDER_LIST_MY_RESET })
+    dispatch({ type: USER_LIST_RESET })
     document.location.href = '/login'
 }
 
