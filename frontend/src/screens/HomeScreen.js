571fce88b06d0ceab5a646cca83d5ad3753eba78
Andrew Brought <andrewbrought@gmail.com>
2020-11-16 17:09:25 -0600

    created paginate component and added it to HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,10 +4,11 @@ import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import {listProducts} from '../actions/productActions';
 
 
-const HomeScreen = ({ match }) => {
+const HomeScreen = ({match}) => {
     const keyword = match.params.keyword
 
     const pageNumber = match.params.pageNumber || 1
@@ -20,7 +21,7 @@ const HomeScreen = ({ match }) => {
     const productList = useSelector(state => state.productList);
     // Here we're destructuring our productList object - it's state is handled in our productReducer
     // Here's where we pull out what we want - so we can display it in our output.
-    const {loading, error, products} = productList;
+    const {loading, error, products, page, pages} = productList;
 
     useEffect(() => {
         // This is firing off the action to get the products and send it down into the state
@@ -31,16 +32,17 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-            <h1>Latest Products</h1>
-            {loading ?
-                <Loader />
-                : error ?
-                    <Message variant='danger'>{error}</Message>
-                    :
+        <h1>Latest Products</h1>
+        {loading ?
+            <Loader/>
+            : error ?
+                <Message variant='danger'>{error}</Message>
+                :
+                <>
                     <Row>
                         {/*We want to loop through our products brought in from product.js
-                so we use .map() to create a list.
-                So we pass in product because we want to show something for each product...for now product.name*/}
+            so we use .map() to create a list.
+            So we pass in product because we want to show something for each product...for now product.name*/}
                         {products.map(product => (
                             // These are media query classes from react-bootstrap to make this component row screen dynamic
                             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -48,8 +50,13 @@ const HomeScreen = ({ match }) => {
                             </Col>
                         ))}
                     </Row>
+                    <Paginate
+                        pages={pages}
+                        page={page}
+                        keyword={keyword ? keyword : ''}
+                    />
+                </>
             }
-
         </>
     );
 }
