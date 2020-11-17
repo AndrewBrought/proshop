9e0aa3a90eae15511c18aea8affa817a12fcb5ff
Andrew Brought <andrewbrought@gmail.com>
2020-11-17 09:01:24 -0600

    brought in carousel component to home screen and updated index.css

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import Product from '../components/Product';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import {listProducts} from '../actions/productActions';
 
 
@@ -32,6 +33,7 @@ const HomeScreen = ({match}) => {
 
     return (
         <>
+            {!keyword && <ProductCarousel />}
         <h1>Latest Products</h1>
         {loading ?
             <Loader/>
