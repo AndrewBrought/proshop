f07c5cd5abf83625a7a56635ae12631fd5f645af
Andrew Brought <andrewbrought@gmail.com>
2020-11-17 21:57:54 -0600

    brought meta component into product screen to dynamically display the item name in browser tag

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
@@ -37,7 +38,7 @@ const HomeScreen = ({match}) => {
         <>
             <Meta />
 
-            {!keyword && <ProductCarousel />}
+            {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
         <h1>Latest Products</h1>
         {loading ?
             <Loader/>
